ae91635a8ff101c30e00edfec6fc65cb2dd499a9
gokuruiz413-cyber <gokuruiz413@gmail.com>
2025-10-06 11:55:46 -0400

    Add files via upload

---
+++ layett.py
@@ -0,0 +1,12 @@
+numero1 = int(input("cual es el primer numero?:"))
+numero2 = int(input("cual es el segundo numero?:"))
+suma = numero1 + numero2 
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+potencia = numero1 ** numero2
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(potencia)
