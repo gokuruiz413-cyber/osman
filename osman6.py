7959521648b7dae97a603c0f4d7dcd7ada55c3a3
gokuruiz413-cyber <gokuruiz413@gmail.com>
2025-10-07 11:31:45 -0400

    Add files via upload

---
+++ osman6.py
@@ -0,0 +1,4 @@
+lista_compañeros = ["Moises Perez", "Nicole Gutierrez", "Heberto Melendes", "Orlaine Rodriguez", "Ema Eguis", "Xavier Lidueña", "Jesus Hernandez", "Jose Carrasquero", "Samuel Alvarado", "Fernando Reverol", "Osman Ruiz", "Raul Hernandez", "Jose Iguaran"]
+
+for nombre in lista_compañeros:
+    print(f"hello, {nombre}")
