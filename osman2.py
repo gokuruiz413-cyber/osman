192983ad91d5f4abc65d6fd61aa113fce18a023b
gokuruiz413-cyber <gokuruiz413@gmail.com>
2025-10-07 09:59:54 -0400

    Add files via upload

---
+++ osman2.py
@@ -0,0 +1,11 @@
+calificacion = 90
+if calificacion > 90 and calificacion >= 100:
+  print("a")
+elif calificacion > 80 and calificacion > 89:
+  print("b")
+elif calificacion > 70 and calificacion > 79:
+  print ("c")
+elif calificacion > 60 and calificacion > 69:
+  print ("d")
+else:
+  print ("f")
