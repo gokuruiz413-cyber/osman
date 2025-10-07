8ca940587d761a3b2a8ce0e3257a97a8aa9eece0
gokuruiz413-cyber <gokuruiz413@gmail.com>
2025-10-07 12:00:40 -0400

    Add files via upload

---
+++ osman8.py
@@ -0,0 +1,10 @@
+mi_diccionario_Osman = {
+    "Nombre": "Osman Ruiz",
+    "Edad": 17,
+    "Fecha de nacimiento": 27/8/2008,
+    "Deporte favorito": "Beisbol", 
+    "personas favoritas": "Familiares y mejores amigos",
+    }
+    
+for clave, valor in mi_diccionario_Osman.items():
+    print(f"\n: {clave},valor:{valor}")
