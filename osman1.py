7ae9daf8ed7bfcaf937a8584c23567ba7f2a296d
gokuruiz413-cyber <gokuruiz413@gmail.com>
2025-10-08 11:55:21 -0400

    Add files via upload

---
+++ osman1.py
@@ -0,0 +1,10 @@
+enunciado = "calcular el area de un rectangulo"
+ancho = 10
+alto = 50
+print(enunciado)
+print("ancho =",ancho)
+print("alto =",alto)
+print("area =?")
+multiplicacion = ancho * alto
+print("ancho * alto = area")
+print("resultado =", multiplicacion)
