f9aa66193e70ac03ea0a93cce1bbb0f77ba14f2f
gokuruiz413-cyber <gokuruiz413@gmail.com>
2025-10-13 11:42:21 -0400

    Add files via upload

---
+++ osman21.py
@@ -0,0 +1,4 @@
+<section class="nombre_de_la_clase">
+    h1 class="titulo>titulo 1<h1>"
+    <button class="foto" src=" https://www.google.com/url?sa=i&url=https%3A%2F%2Flga.lagranaldea.com%2F2023%2F09%2F15%2Fel-beisbol-en-venezuela-es-una-historia-de-exitos%2F&psig=AOvVaw3f5zkVKTJEeWVIfguo_oSN&ust=1760454996847000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCODExoi8oZADFQAAAAAdAAAAABAV"
+   </section>  
