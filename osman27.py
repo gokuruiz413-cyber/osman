ca5b9298dfcde01bd90e9e80d18fa4d2261b2951
gokuruiz413-cyber <gokuruiz413@gmail.com>
2025-10-14 10:18:26 -0400

    Add files via upload

---
+++ osman27.py
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <title></title>
+
+
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+
+<div>
+    <table>
+    <1-- encabezado
+    <th>osman</th> 
+    <th>17</th>
+    <th>venezuela</th>
+</thaad>
+
