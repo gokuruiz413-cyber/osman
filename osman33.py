671c637dbb442e976c4e1c1dd425bcae496149a7
gokuruiz413-cyber <gokuruiz413@gmail.com>
2025-10-16 11:06:32 -0400

    Add files via upload

---
+++ osman33.py
@@ -0,0 +1,6 @@
+
+"const"("tablet") **"10 pulgadas"**
+"let" ("precio") **450.99**
+"let" cantidad ("stock") **25**
+"const" pruducto que si tiene es gratis ("enviogratis") **true**
+"tablet" 10 pulgadas 
