b0bf0ddb43999f5f22da860e40cd2f86e7ca1acc
gokuruiz413-cyber <gokuruiz413@gmail.com>
2025-10-22 16:55:46 -0400

    Add files via upload

---
+++ goku.cs
@@ -0,0 +1,14 @@
+using system;
+
+ class goku
+{
+    static void main() {
+        console.write("numero? ");
+        string numero = console.readline():
+       if edad = int.parse(entrada):
+       
+       if(numero % 2 == 0) {
+           console.write("par")
+           
+           else if(numero)% 2 == 0
+       }
